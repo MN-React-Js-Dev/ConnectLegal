00dce90afe5dbc90a9c11545fc0b1f34a4e2496b
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-01-04 10:56:51 +0530

    First c

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/fontawesome.css'
+import './assets/css/templatemo-space-dynamic.css'
+import './assets/css/animated.css'
+import './assets/css/owl.css'
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import Header from './Component/Navbar/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
