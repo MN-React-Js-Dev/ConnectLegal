05ff89726f91955760c6da2eb5c284da2cc9ff06
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-01-05 17:14:39 +0530

    Shub commit

--- src/App.js
+++ src/App.js
@@ -4,40 +4,37 @@ import './assets/css/fontawesome.css'
 import './assets/css/templatemo-space-dynamic.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
 import Header from './Component/Navbar/Header';
-<<<<<<< HEAD
 import Footer from './Component/Navbar/Footer';
 import LookingForLawyerSection from './Component/LookingForLawyerSection';
 import WhyRegister from './Component/WhyRegister';
 import ServicesContainer from './Component/ServicesContainer';
 import FindLeagalServices from './Component/FindLeagalServices';
-=======
 import FirstContainer from './Component/FirstContainer';
 import SecondContainer from './Component/SecondContainer';
 import Thirdcontainer from './Component/Thirdcontainer';
->>>>>>> eb8debc8ec4af98623d553a4dbe24e746fb8ef82
 
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
+      <br />
+      <br />
+      <br />
+      <br />
+      <FirstContainer />
+      <SecondContainer />
+      <Thirdcontainer />
+      <FindLeagalServices />
       <LookingForLawyerSection />
-      <FindLeagalServices/>
       {/* <WhyRegister /> */}
       {/* <ServicesContainer /> */}
+      <br />
+      <br />
+      <br />
+      <br />
       <Footer />
-=======
-         <Header/>
-         <br/>  
-         <br/> 
-         <br/> 
-         <br/>    
-    <FirstContainer/>
-    <SecondContainer/>
-    <Thirdcontainer/>
- 
->>>>>>> eb8debc8ec4af98623d553a4dbe24e746fb8ef82
+
     </>
   );
 }
