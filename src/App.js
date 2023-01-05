89ca6dc23b9ac259fa2ec19a9b8a07b9bbb57bba
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-01-05 17:38:33 +0530

    Sejal commit

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import './assets/css/fontawesome.css'
 import './assets/css/templatemo-space-dynamic.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
 import Header from './Component/Navbar/Header';
-<<<<<<< HEAD
 import Footer from './Component/Navbar/Footer';
 import LookingForLawyerSection from './Component/LookingForLawyerSection';
 import WhyRegister from './Component/WhyRegister';
-import ServicesContainer from './Component/ServicesContainer';
 import FindLeagalServices from './Component/FindLeagalServices';
 import FirstContainer from './Component/FirstContainer';
 import SecondContainer from './Component/SecondContainer';
 import Thirdcontainer from './Component/Thirdcontainer';
-=======
 import ClientContainer from './Component/ClientContainer';
 import ProServicesContainer from './Component/ProServicesContainer';
->>>>>>> f72e448df44f0be42ee64967963cf6cb979c2553
 
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
       <br />
       <br />
@@ -35,17 +30,10 @@ function App() {
       <LookingForLawyerSection />
       {/* <WhyRegister /> */}
       {/* <ServicesContainer /> */}
-      <br />
-      <br />
-      <br />
-      <br />
-      <Footer />
-
-=======
-      <Header/>
+      
       <ProServicesContainer />
       <ClientContainer />
->>>>>>> f72e448df44f0be42ee64967963cf6cb979c2553
+      <Footer />
     </>
   );
 }
--- src/Component/ProServicesContainer.js
+++ src/Component/ProServicesContainer.js
@@ -7,49 +7,51 @@ import rightsignBrown from '../assets/images/vector-right-brown.png'
 
 const ProServicesContainer = () => {
   return (
-  <div Class="container mainContainer">
+    <section className='prov-services_conatiner'>
+      <div Class="container mainContainer">
         <div Class="row innerContainer">
-            <div Class="col-sm-12 left-Container">
-              <div className='pro-services'>PRO Services</div>
-              <div className='appointment'>Make an appointment with <strong>Advocates and Legal</strong><br/> consultancy, Today! </div>
-                <div className='appointment-text'>
-                <div className='right-sign'>
-                    <img src={rigthsign} alt="right" />
-                    <img src={rigthsign} alt="right" />
-                    <img src={rigthsign} alt="right" />
-                    <img src={rigthsign} alt="right" />  
-                  </div>
-                  <div className='appointment-grp'>
-                    <img src={appointment} alt="appointment" />
-                  </div>
-                  <div className='arrow-img'>
-                    <img src={arrowimg} alt="appointment" />
-                  </div>
+          <div Class="col-sm-12 left-Container">
+            <div className='pro-services'>PRO Services</div>
+            <div className='appointment'>Make an appointment with <strong>Advocates and Legal</strong><br /> consultancy, Today! </div>
+            <div className='appointment-text'>
+              <div className='right-sign'>
+                <img src={rigthsign} alt="right" />
+                <img src={rigthsign} alt="right" />
+                <img src={rigthsign} alt="right" />
+                <img src={rigthsign} alt="right" />
               </div>
-              
+              <div className='appointment-grp'>
+                <img src={appointment} alt="appointment" />
+              </div>
+              <div className='arrow-img'>
+                <img src={arrowimg} alt="appointment" />
+              </div>
             </div>
-           
 
-            <div Class="col-sm-12 right-Container">
-            <div className='legal-header'>Legal Translation <br/> Services</div>
-              <div className='appointment'><strong>Make an appointment</strong> with Advocates and <br/> Legal consultancy, Today! </div>
-                <div className='appointment-text'>
-                  <div className='right-sign'>
-                    <img src={rightsignBrown} alt="right" />
-                    <img src={rightsignBrown} alt="right" />
-                    <img src={rightsignBrown} alt="right" />
-                    <img src={rightsignBrown} alt="right" />  
-                  </div>
-                  <div className='appointment-grp'>
-                    <img src={appointment} alt="appointment" />
-                  </div>
-                  <div className='arrow-img'>
-                  <img src={arrowimg} alt="appointment" />
-                  </div>
+          </div>
+
+
+          <div Class="col-sm-12 right-Container">
+            <div className='legal-header'>Legal Translation <br /> Services</div>
+            <div className='appointment'><strong>Make an appointment</strong> with Advocates and <br /> Legal consultancy, Today! </div>
+            <div className='appointment-text'>
+              <div className='right-sign'>
+                <img src={rightsignBrown} alt="right" />
+                <img src={rightsignBrown} alt="right" />
+                <img src={rightsignBrown} alt="right" />
+                <img src={rightsignBrown} alt="right" />
+              </div>
+              <div className='appointment-grp'>
+                <img src={appointment} alt="appointment" />
+              </div>
+              <div className='arrow-img'>
+                <img src={arrowimg} alt="appointment" />
               </div>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
+    </section>
   )
 }
 
