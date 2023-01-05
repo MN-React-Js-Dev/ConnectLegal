eb8debc8ec4af98623d553a4dbe24e746fb8ef82
ajaykoliproginter <ajay.koli.5057@gmail.com>
2023-01-05 16:54:22 +0530

    resolve issue

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import './assets/css/animated.css'
 import './assets/css/owl.css'
 import './assets/vendor/bootstrap/css/bootstrap.min.css'
 import Header from './Component/Navbar/Header';
+import FirstContainer from './Component/FirstContainer';
+import SecondContainer from './Component/SecondContainer';
+import Thirdcontainer from './Component/Thirdcontainer';
 
 
 function App() {
   return (
     <>
-      <Header/>
+         <Header/>
+         <br/>  
+         <br/> 
+         <br/> 
+         <br/>    
+    <FirstContainer/>
+    <SecondContainer/>
+    <Thirdcontainer/>
+ 
     </>
   );
 }
--- src/Component/SecondContainer.js
+++ src/Component/SecondContainer.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import "../assets/css/second-container.css"
-import img3 from "../assets/images/Group 300.jpg"
-import img1 from "../assets/images/Group 65.jpg"
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../assets/css/second-container.css";
+import img3 from "../assets/images/Group 300.jpg";
+import img1 from "../assets/images/Group 65.jpg";
 function Secondcontainer() {
   return (
     <>
-   
-    <Container className='main-container-data'>
-      <Row>
-        <Col className='col-7'>
-      <h2>We connect clients and Legal Consultants in one platform.</h2>
-      <p>Make an appointment with Advocates and Legal consultancy,
-         Today! or chat with a lawyer online for free in Dubai and across
-        UAE now. We provide legal Services by connecting you with the
-        Legal Consultants, Or you can even hire a Lawyer for your Legal
-        issues. You can also read our blogs which provides some of the
-        vital informations.</p>
-        <img class="w-50 p-3" src={img1}  />
-    </Col>
-      <Col className='col-5'>  
-        <img  src={img3}  />
-         </Col>
-      </Row>
-    </Container>
+      <section className="we_coonect_section">
+        <Container className="main-container-data">
+          <Row>
+            <Col className="col-7 cstm_col_we_connect">
+              <h2>
+                <span className="first-container-test">
+                  We connect clients and <br />Legal  
+                </span> Consultants in one platform.
+              </h2>
+              <p className="second_container_para">
+                Make an appointment with Advocates and Legal consultancy, Today!
+                or chat with a lawyer online for free in Dubai and across UAE
+                now. We provide legal Services by connecting you with the Legal
+                Consultants, Or you can even hire a Lawyer for your Legal
+                issues. You can also read our blogs which provides some of the
+                vital informations.
+              </p>
+              <img class="cstm_img_btn" src={img1} />
+            </Col>
+            <Col className="col-5">
+              <img src={img3} />
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
-  )
+  );
 }
 
-export default Secondcontainer
+export default Secondcontainer;
