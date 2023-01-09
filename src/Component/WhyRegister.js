163806bd1163782285550ba18ce2325097506627
MN-React-Js-Dev <mayur.nandwalker.5057@gmail.com>
2023-01-09 11:03:32 +0530

    Designing chnages

--- src/Component/WhyRegister.js
+++ src/Component/WhyRegister.js
@@ -12,54 +12,7 @@ import "../assets/css/why-register.css"
 
 const WhyRegister = () => {
   return (
-    <>
-      {/* <section className='prov-services_conatiner-1'>
-        <div Class="container mainContainer-1">
-          <div Class="row innerContainer">
-            <div class="col-md-6">
-              <div className='col-cst-est'>
-                <img src={Are_You} className='Are_You' />
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div className='img-container'>
-                <form className='form-getstart'>
-                  <h1 className='Heading'>Get started</h1>
-                  <a className='Headingsub'>Alredy have account? <e style={{ color: "#3DC9A1" }}>sign in</e></a>
-                  <br />
-                  <br />
-                  <div>
-                    <lable className='lableclass'>Name</lable>
-                    <br />
-                    <input className='Inputclass' type='text' placeholder='Enter your name'></input>
-                  </div>
-                  <br />
-                  <div>
-                    <lable className='lableclass'>Email Address</lable>
-                    <br />
-                    <input className='Inputclass' type='email' placeholder='email@domaim.com' />
-                  </div>
-                  <br />
-                  <div>
-                    <lable className='lableclass'>Password </lable>
-                    <br />
-                    <input className='Inputclass' type='password' /><img className="eye" src={eye} />
-                  </div>
-                  <br />
-                  <a className='f_pasword' >forgot Password</a>
-                  <br />
-                  <br />
-                  <br />
-                  <button className='Sign_button'>Sign Up</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
-
-
-      <section className='prov-services_conatiner-ma'>
+    <> <section className='prov-services_conatiner-ma'>
         <div className='conatainer hi_class'>
           <div className='row'>
             <div className='col-sm-6'> <img src={Are_You} className='Are_Yousdf' /></div>
